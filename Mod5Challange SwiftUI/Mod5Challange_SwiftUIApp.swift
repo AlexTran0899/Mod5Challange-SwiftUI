d8fc3c9744635a3cb1ecb7f1de1ce126420c2660
Alextran0899 <Alextran0899@gmail.com>
2023-03-25 08:45:23 -0500

    Initial Commit

---
+++ Mod5Challange SwiftUI/Mod5Challange_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mod5Challange_SwiftUIApp.swift
+//  Mod5Challange SwiftUI
+//
+//  Created by Alex Tran on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Mod5Challange_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
